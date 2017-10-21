147393f93b24535c66304510004e3933262f5c5d
Chance Brilz <chancebrilz@gmail.com>
2017-10-21 15:03:08 -0400

    updated test script

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this sh script is MacOS specific and requires blueutil,
+# a 3rd party app: http://www.frederikseiffert.de/blueutil/
+
 if blueutil status | grep 'Status: on' > /dev/null; then
 	blueutil off
 else
